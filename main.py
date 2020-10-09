a1e91f53dff487dc282875aba569846a2de30086
rxhan <rxhan@wolke7.net>
2020-10-09 20:12:34 +0200

    doppeltes Logging entfernt

--- main.py
+++ main.py
@@ -43,22 +43,6 @@ from e3dc.rscp_type import RSCPType
 from e3dcwebgui import E3DCWebGui
 from gui import MainFrame
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
 try:
     import thread
 except ImportError:
